0412a33effc03b3cd5c5b3110de13dd1398769f3
Necmettin <necmettin_kzldg@hotmail.com>
2024-01-13 15:23:18 +0100

    Mouse over is finished

--- cypress/e2e/0-MyCypress/day_04/MouseOver.cy.js
+++ cypress/e2e/0-MyCypress/day_04/MouseOver.cy.js
@@ -4,5 +4,17 @@ describe('Hover', () => {
         cy.visit('https://www.amazon.com')
         cy.get('.icp-nav-link-inner > .nav-line-2').trigger('mouseover')
         cy.contains('Change country/region.').click()
+        cy.get('.a-dropdown-container > .a-button > .a-button-inner > .a-button-text').click()
+        cy.get('#icp-dropdown_18').click()
+        cy.get('.a-button-input').click()
+
+    });
+
+    it.only('', () => {
+        cy.visit('https://www.amazon.com')
+        cy.wait(4000)
+        cy.reload() // sayfayi yenilerken cache den yukler
+        cy.wait(4000)
+        cy.reload(true) // sayfayi cache den yuklemez sifirdan yukler
     });
 });
